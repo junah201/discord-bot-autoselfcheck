f0d65f679cd70286b345dfd2bc1ea7f0345c31f4
white201 <white_201@naver.com>
2021-05-31 11:06:34 +0900

    Add files via upload

--- auto_self_check.py
+++ auto_self_check.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+#-*- coding: UTF8-*-
 
 import discord
 from discord.ext import commands, tasks
@@ -15,11 +15,11 @@ bot = commands.Bot(command_prefix='?')
 #KST = datetime.timezone(datetime.timedelta(hours=9))
 
 start_minute=9
-last_day = ""
+last_day = "2021-05-29"
 #start_minute=datetime.datetime.now().minute
 
 end_msg = "\n\n개발자 : white#0201 | [개발자 서버](https://discord.gg/bhJEbEgHED) | [초대링크](https://discord.com/api/oauth2/authorize?client_id=846650618701283359&permissions=0&scope=bot)"
-#end_msg = "\n\n봇 : 자동자가진단#4767 | 개발자 : white#0201 | [개발자 서버](https://discord.gg/bhJEbEgHED) | [초대링크](https://discord.com/api/oauth2/authorize?client_id=846650618701283359&permissions=0&scope=bot)"
+#end_msg = "\n\n봇 : 자동자가진단#4767 | 개발자 : white#0201 | [개발자 서버](https://discord.gg/bhJEbEgHED) | [초대링크](https://discord.com/api/oauth2/authorize?client_id=846650618701283359?permissions=0?scope=bot)"
 json_file_name = "user_data.json"
 
 log_add_success_channel = "847490299638186054"
@@ -35,9 +35,10 @@ log_json_backup_channel = "847830055916404766"
 async def auto_self_check():
     global start_minute
     global last_day
-    print("[{}] 무한루프가 돌아가는 중...".format(datetime.datetime.now()))
-    if datetime.datetime.now().hour == 7 and datetime.datetime.now().minute == start_minute and last_day != datetime.datetime.now().strftime('%Y-%m-%d'):
-        with open(json_file_name, "r",encoding='UTF-8') as json_file:
+    print(f"[{datetime.datetime.now()}] 무한루프가 돌아가는 중...")
+    if datetime.datetime.now().hour == 7 and datetime.datetime.now().minute == start_minute and last_day != datetime.datetime.now().strftime('%Y-%m-%d') and datetime.datetime.today().weekday()<5:
+        await user_data_backup()
+        with open(json_file_name, "r",encoding='utf-8-sig') as json_file:
             user_data=json.load(json_file)
         
         start_minute=random.randrange(1,16)
@@ -73,9 +74,14 @@ async def on_error(event, *args, **kwargs):
     await user.send(f"에러 발생 : {event} : {str(exc[0].__name__)} : {str(exc[1])}")
 
 async def user_data_backup():
-    with open(json_file_name, "r",encoding='UTF-8') as json_file:
-            user_data=json.load(json_file)
-    await send_log(log_json_backup_channel,"```"+str(user_data)+"```")
+    channel = bot.get_channel(int(log_json_backup_channel))
+    await channel.send(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]")
+
+    file = discord.File("user_data.json")
+    await channel.send(file=file)
+    file = discord.File("auto_self_check.py")
+    await channel.send(file=file)
+
 
 
 async def send_embed_log(channel,embed):
@@ -104,7 +110,7 @@ async def send_DM(data,user_id,start_minute,user_data):
             print("자가진단 성공 후 메시지 발송 완료...")
         else:
             now = datetime.datetime.now()
-            embed = discord.Embed(title="자가 진단 실패", description="[{}] 부로 {} 님의\n자가진단이 실시되었습니다만 자가진단에 실패하셨습니다.\n이름 : {}\n생년월일 : {}\n지역 : {}\n학교 이름 : {}\n학교 타입 : {}\n비밀번호 : {}**\n\n{}{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),user_data[user_id]["name"],user_data[user_id]["name"],user_data[user_id]["birth"],user_data[user_id]["area"],user_data[user_id]["school_name"],user_data[user_id]["school_type"],user_data[user_id]["passward"][:2],data["message"],end_msg), color=0x62c1cc)
+            embed = discord.Embed(title="자가 진단 실패", description="[{}] 부로 [{}] 님의\n자가진단이 실시되었습니다만 자가진단에 실패하셨습니다.\n이름 : {}\n생년월일 : {}\n지역 : {}\n학교 이름 : {}\n학교 타입 : {}\n비밀번호 : {}**\n\n{}{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),user_data[user_id]["name"],user_data[user_id]["name"],user_data[user_id]["birth"],user_data[user_id]["area"],user_data[user_id]["school_name"],user_data[user_id]["school_type"],user_data[user_id]["passward"][:2],data["message"],end_msg), color=0x62c1cc)
             await user.send(embed=embed)
             embed = discord.Embed(title="자가 진단 실패",description="[{}]\n<@{}>님의 자가진단 실패\n{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),user_id,data["message"]), color=0x62c1cc)
             await send_embed_log(log_auto_self_check_failure_channel,embed)
@@ -118,7 +124,7 @@ async def send_DM(data,user_id,start_minute,user_data):
 @bot.command()
 async def 도움말(ctx):
     embed = discord.Embed(title="도움말", description="자동자가진단 봇에 대한 도움말 입니다.",color=0x62c1cc)
-    embed.add_field(name="?정보등록", value="?정보등록 [이름] [생년월일] [지역] [학교이름] [학교타입] [비밀번호]```?정보등록 홍길동 721027 서울시 길동고 고등학교 1234``````?정보등록 홍길동 050201 충청남도 길동중 중학교 2580```", inline=False)
+    embed.add_field(name="?정보등록", value="?정보등록 [이름] [생년월일] [지역] [학교이름] [학교타입] [비밀번호]```?정보등록 홍길동 721027 서울시 길동고 고등학교 1234``````?정보등록 홍길동 050201 충청남도 길동중 중학교 2580```정보등록은 개인DM으로 하는 것을 보안상 추천 드립니다.", inline=False)
     embed.add_field(name="?정보삭제", value="?정보삭제\n※디스코드 아이디를 기준으로 삭제합니다.\n※만약 디스코드 계정이 바뀌었을 경우 white201#0201 님께 문의 부탁드립니다.", inline=False)
     embed.add_field(name="?정보확인", value="?정보확인\n※디스코드 아이디를 기준으로 확인합니다.\n※만약 디스코드 계정이 바뀌었을 경우 white201#0201 님께 문의 부탁드립니다.", inline=False)
     embed.add_field(name="기타",value="자동자가진단은 7시 00분에서 7시 16분 사이에 랜덤하게 작동하며,\n자동자가진단 DM 메시지를 통하여 그 다음날의 작동 시간을 알 수 있습니다.", inline=False)
@@ -128,7 +134,7 @@ async def 도움말(ctx):
 @bot.command()
 async def 명령어(ctx):
     embed = discord.Embed(title="도움말", description="자동자가진단 봇에 대한 도움말 입니다.",color=0x62c1cc)
-    embed.add_field(name="?정보등록", value="?정보등록 [이름] [생년월일] [지역] [학교이름] [학교타입] [비밀번호]```?정보등록 홍길동 721027 서울시 길동고 고등학교 1234``````?정보등록 홍길동 050201 충청남도 길동중 중학교 2580```", inline=False)
+    embed.add_field(name="?정보등록", value="?정보등록 [이름] [생년월일] [지역] [학교이름] [학교타입] [비밀번호]```?정보등록 홍길동 721027 서울시 길동고 고등학교 1234``````?정보등록 홍길동 050201 충청남도 길동중 중학교 2580```※정보등록은 개인DM으로 하는 것을 보안상 추천 드립니다.", inline=False)
     embed.add_field(name="?정보삭제", value="?정보삭제\n※디스코드 아이디를 기준으로 삭제합니다.\n※만약 디스코드 계정이 바뀌었을 경우 white201#0201 님께 문의 부탁드립니다.", inline=False)
     embed.add_field(name="?정보확인", value="?정보확인\n※디스코드 아이디를 기준으로 확인합니다.\n※만약 디스코드 계정이 바뀌었을 경우 white201#0201 님께 문의 부탁드립니다.", inline=False)
     embed.add_field(name="기타",value="자동자가진단은 7시 00분에서 7시 16분 사이에 랜덤하게 작동하며,\n자동자가진단 DM 메시지를 통하여 그 다음날의 작동 시간을 알 수 있습니다.", inline=False)
@@ -148,7 +154,7 @@ async def 서버목록(ctx):
 @bot.command()
 async def 정보등록(ctx,name=None,birth=None,area=None,school_name=None,school_type=None,passward=None):
     if name!=None and birth!=None and area!=None and school_name!=None and school_type!=None and passward!=None:
-        with open(json_file_name, "r",encoding='UTF-8') as json_file:
+        with open(json_file_name, "r",encoding='utf-8-sig') as json_file:
             user_data=json.load(json_file)
         user_data[str(ctx.author.id)] = {
             "uesr_id":str(ctx.author.id),
@@ -172,7 +178,7 @@ async def 정보등록(ctx,name=None,birth=None,area=None,school_name=None,schoo
 
         user = await bot.fetch_user(ctx.author.id)
         if user is not None:
-            embed = discord.Embed(title="정보 등록 완료[보안메시지]", description="{} 부로 {} 님의 정보 등록이 완료되었습니다.\n이름 : {}\n생년월일 : {}\n지역 : {}\n학교 이름 : {}\n학교 타입 : {}\n비밀번호 : {}**{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),user_data[str(ctx.author.id)]["name"],user_data[str(ctx.author.id)]["name"],user_data[str(ctx.author.id)]["birth"],user_data[str(ctx.author.id)]["area"],user_data[str(ctx.author.id)]["school_name"],user_data[str(ctx.author.id)]["school_type"],user_data[str(ctx.author.id)]["passward"][:2],end_msg), color=0x62c1cc)
+            embed = discord.Embed(title="정보 등록 완료[보안메시지]", description="{} 부로 {} 님의 정보 등록이 완료되었습니다.\n```이름 : {}\n생년월일 : {}\n지역 : {}\n학교 이름 : {}\n학교 타입 : {}\n비밀번호 : {}**{}```".format(now.strftime('%Y-%m-%d %H:%M:%S'),user_data[str(ctx.author.id)]["name"],user_data[str(ctx.author.id)]["name"],user_data[str(ctx.author.id)]["birth"],user_data[str(ctx.author.id)]["area"],user_data[str(ctx.author.id)]["school_name"],user_data[str(ctx.author.id)]["school_type"],user_data[str(ctx.author.id)]["passward"][:2],end_msg), color=0x62c1cc)
             await user.send(embed=embed)
         else:
             user = await bot.fetch_user(523017072796499968)
@@ -185,29 +191,30 @@ async def 정보등록(ctx,name=None,birth=None,area=None,school_name=None,schoo
         
 @bot.command()
 async def 정보삭제(ctx):
-    with open(json_file_name, "r",encoding='UTF-8') as json_file:
+    with open(json_file_name, "r",encoding='utf-8-sig') as json_file:
         user_data=json.load(json_file)
 
     if user_data.get(str(ctx.author.id)):
         data = user_data.pop(str(ctx.author.id))
         
-        with open(json_file_name, "w",encoding='UTF-8') as json_file:
+        with open(json_file_name, "w",encoding='utf-8-sig') as json_file:
             json.dump(user_data,json_file,ensure_ascii = False, indent=4)
 
         now = datetime.datetime.now()
-        embed = discord.Embed(title="정보 삭제 완료", description="{} 부로 {} 님의 정보 삭제가 완료되었습니다.\n구체적인 삭제 정보는 개인DM을 확인해주세요.{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),data["name"],end_msg), color=0x62c1cc)
+        embed = discord.Embed(title="정보 삭제 완료", description="[{]} 부로 {} 님의 정보 삭제가 완료되었습니다.\n구체적인 삭제 정보는 개인DM을 확인해주세요.{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),data["name"],end_msg), color=0x62c1cc)
         await ctx.send(embed=embed)
         user = await bot.fetch_user(str(ctx.author.id))
 
-        embed = discord.Embed(title="정보 삭제 완료[보안메시지]", description="{} 부로 {} 님의 정보 삭제가 완료되었습니다.\n이름 : {}\n생년월일 : {}\n지역 : {}\n학교 이름 : {}\n학교 타입 : {}\n비밀번호 : {}**{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),data["name"],data["name"],data["birth"],data["area"],data["school_name"],data["school_type"],data["passward"][:2],end_msg), color=0x62c1cc)
+        embed = discord.Embed(title="정보 삭제 완료[보안메시지]", description="[{}] 부로 {} 님의 정보 삭제가 완료되었습니다.\n이름 : {}\n생년월일 : {}\n지역 : {}\n학교 이름 : {}\n학교 타입 : {}\n비밀번호 : {}**{}".format(now.strftime('%Y-%m-%d %H:%M:%S'),data["name"],data["name"],data["birth"],data["area"],data["school_name"],data["school_type"],data["passward"][:2],end_msg), color=0x62c1cc)
         await user.send(embed=embed)
     else:
-        embed = discord.Embed(title="정보 삭제 실패", description="[{}] 에  [{}] 님의 정보 삭제를 실패하셨습니다.\n디스코드 아이디[{}]로 등록된 정보가 없습니다.\n구체적인 문의는 관리자[white201#0201]님께 문의 부탁드립니다.".format(now.strftime('%Y-%m-%d %H:%M:%S'),data["name"],ctx.author.id,end_msg), color=0x62c1cc)
+        now = datetime.datetime.now()
+        embed = discord.Embed(title="정보 삭제 실패", description="[{}] 에  <@{}> 님의 정보 삭제를 실패하셨습니다.\n디스코드 아이디[{}]로 등록된 정보가 없습니다.\n구체적인 문의는 관리자[white201#0201]님께 문의 부탁드립니다.".format(now.strftime('%Y-%m-%d %H:%M:%S'),ctx.author.id,ctx.author.id,end_msg), color=0x62c1cc)
         await ctx.send(embed=embed)
-        
+
 @bot.command()
 async def 정보확인(ctx):
-    with open(json_file_name, "r",encoding='UTF-8') as json_file:
+    with open(json_file_name, "r",encoding='utf-8-sig') as json_file:
         user_data=json.load(json_file)
 
     if user_data.get(str(ctx.author.id)):
@@ -221,31 +228,26 @@ async def 정보확인(ctx):
             user = await bot.fetch_user(523017072796499968)
             await user.send("DM 보내기가 정상적으로 처리되지 않아서 관리자에게 로그 DM을 보냈습니다.")
     else:
-        embed = discord.Embed(title="정보 확인 실패",description=f"디스코드 아이디에 해당하는 데이터가 없습니다. 관리자에게 문의 부탁드립니다.{end_msg}", color=0x62c1cc)
+        embed = discord.Embed(title="정보 확인 실패",description=f"디스코드 아이디에 해당하는 데이터가 없습니다. 관리자에게 문의 부탁드립니다.{end_msg})", color=0x62c1cc)
         await ctx.send(embed=embed)
   
 @bot.command()
 async def 관리자정보확인(ctx):
     if str(ctx.author.id) == '523017072796499968':
-        with open(json_file_name, "r",encoding='UTF-8') as json_file:
-            user_data=json.load(json_file)
-        embed = discord.Embed(title="관리자 정보 확인", description=f"개인DM으로 정보를 보냈습니다.{end_msg})", color=0x62c1cc)
-        await ctx.send(embed=embed)
-        user = await bot.fetch_user(str(ctx.author.id))
-        if user is not None:
-            embed = discord.Embed(title="관리자 정보 확인[보안메시지]", description="서버 위치 : {}\n\n```{}```".format(host_name,str(user_data)), color=0x62c1cc)
-            await user.send(embed=embed)
-        else:
-            user = await bot.fetch_user(523017072796499968)
-            await user.send("DM 보내기가 정상적으로 처리되지 않아서 관리자에게 로그 DM을 보냈습니다.")
+        channel = bot.get_channel(int(log_json_backup_channel))
+        await channel.send(f"[{datetime.datetime.now().strftime('%Y-%m-%d')}]\n{host_name}")
+        file = discord.File("user_data.json")
+        await channel.send(file=file)
+        #file = discord.File("auto_self_check.py")
+        #await channel.send(file=file)
     else:
         user = await bot.fetch_user(ctx.author.id)
         await user.send("관리자 권한이 없어 해당 명령어를 사용할 수 없습니다.")
-
+       
 @bot.command()
 async def 관리자전체자가진단(ctx):
     if str(ctx.author.id) == '523017072796499968':
-        with open(json_file_name, "r",encoding='UTF-8') as json_file:
+        with open(json_file_name, "r",encoding='utf-8-sig') as json_file:
             user_data=json.load(json_file)
         global start_minute
         start_minute=random.randrange(1,16)
@@ -266,7 +268,7 @@ async def 관리자전체자가진단(ctx):
 @bot.command()
 async def 관리자전체공지(ctx,*,msg):
     if str(ctx.author.id) == '523017072796499968':
-        with open(json_file_name, "r",encoding='UTF-8') as json_file:
+        with open(json_file_name, "r",encoding='utf-8-sig') as json_file:
             user_data=json.load(json_file)
         for user_id in user_data.keys():
             user = await bot.fetch_user(user_id)
@@ -274,6 +276,6 @@ async def 관리자전체공지(ctx,*,msg):
     else:
         user = await bot.fetch_user(ctx.author.id)
         await user.send("관리자 권한이 없어 해당 명령어를 사용할 수 없습니다.")
-
-token = os.environ["BOT_TOKEN"]
+token = 'ODQ2NjUwNjE4NzAxMjgzMzU5.YKym1Q.AdgTSMXe4mtn41btGR5X9_INM-4'
+#token = 'Nzk4NDI0NTk5NDk4MDYzODky.X_002g.Wgrp8Mr_gfpJrhBbd4UQHAUkVVo'
 bot.run(token)
