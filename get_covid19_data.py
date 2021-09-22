eef3950f71ec1dd16f5c7ce76da06e1f3962ee9c
white201 <white201.dev@gmail.com>
2021-09-22 16:16:55 +0900

    API 키 비공개 처리

--- get_covid19_data.py
+++ get_covid19_data.py
@@ -1,27 +1,12 @@
 import requests
 import xmltodict
-import json
+import json, os
 import asyncio
 import datetime
+from dotenv import load_dotenv
 
-covid19_api_key = "PEk93HmpBu4oPQ7khzVYY8z8f2JCUG9UIOHRrXKJB11j%2BfscZ4AoG%2F7z5pEr5c4BoQ7Ld46hGk2dzWEGS2PEIg%3D%3D"
-covid19_api_key = "PEk93HmpBu4oPQ7khzVYY8z8f2JCUG9UIOHRrXKJB11j%2BfscZ4AoG%2F7z5pEr5c4BoQ7Ld46hGk2dzWEGS2PEIg%3D%3D"
-seoul_covid19_api_key = "4e7a51565377686938376e73627150"
-
-'''
-async def get_covid19_decide():
-    day = datetime.datetime.now().strftime('%Y%m%d')
-    old_day = (datetime.datetime.now()-datetime.timedelta(days=1)).strftime('%Y%m%d')
-    url = f'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey={covid19_api_key}&Type=json&pageNo=1&numOfRows=10&startCreateDt={old_day}&endCreateDt={day}'
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-    print(url)
-    response = requests.get(url,headers=headers)
-    covid19_data = json.loads(json.dumps(xmltodict.parse(response.text), indent=4))
-    if len(covid19_data['response']['body']['items']['item']) == 2:
-        return int(covid19_data['response']['body']['items']['item'][0]['decideCnt']) - int(covid19_data['response']['body']['items']['item'][1]['decideCnt'])
-    else:
-        return None
-'''
+load_dotenv()
+covid19_api_key = os.getenv('COVID_API_KEY')
 
 async def get_covid19_decide():
     day = (datetime.datetime.now()-datetime.timedelta(days=1)).strftime('%Y%m%d')
