d8a60fa80b3192bbbb2fed23b345de44c87d992d
white201 <junah.dev@gmail.com>
2021-10-28 14:45:41 +0900

    change_presence 에러 수정

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import koreanbots,os
 from variable import *
 
 bot = commands.Bot(command_prefix=PREFIX)
+game = discord.Game(f" {PREFIX}명령어 | {len(user_data.keys())}명의 자가진단을 처리 ")
+await self.bot.change_presence(status=discord.Status.online, activity=game)
+
 for filename in os.listdir('./Cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'Cogs.{filename[:-3]}')
