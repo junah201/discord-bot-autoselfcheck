db858d6590113c3385a216742fe2180fdd7b2568
white201 <junah.dev@gmail.com>
2021-11-05 23:18:37 +0900

    변수 저장 위치 변경

--- get_school_data.py
+++ get_school_data.py
@@ -1,6 +1,7 @@
 import requests
 import json, os
 import asyncio
+from variable import *
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,36 +21,15 @@ async def get_school_code(school_name:str,area_code:str):
             #학교코드
             school_code = school_data['schoolInfo'][1]["row"][0]['SD_SCHUL_CODE']
             return school_code
-            pass
         except:
             return None
-            pass
     else:
         return None
-        pass
 
 #지역 코드 리턴 함수
 async def get_area_code(school_area:str==None):
     if school_area == None:
         return None
-
-    Souel = ['서울', '서울시', '서울교육청', '서울시교육청', '서울특별시']
-    Busan = ['부산', '부산광역시', '부산시', '부산교육청', '부산광역시교육청']
-    Deagu = ['대구', '대구광역시', '대구시', '대구교육청', '대구광역시교육청']
-    Incheon = ['인천', '인천광역시', '인천시', '인천교육청', '인천광역시교육청']
-    Gwangju = ['광주', '광주광역시', '광주시', '광주교육청', '광주광역시교육청']
-    Daejeon = ['대전', '대전광역시', '대전시', '대전교육청', '대전광역시교육청']
-    Ulsan = ['울산', '울산광역시', '울산시', '울산교육청', '울산광역시교육청']
-    Sejong = ['세종', '세종특별시', '세종시', '세종교육청', '세종특별자치시', '세종특별자치시교육청']
-    Gyeonggi = ['경기', '경기도', '경기교육청', '경기도교육청']
-    Gangwon = ['강원', '강원도', '강원교육청', '강원도교육청']
-    Chungcheongbuk = ['충북', '충청북도', '충북교육청', '충청북도교육청']
-    Chungcheongnam = ['충남', '충청남도', '충남교육청', '충청남도교육청']
-    Jeollabuk = ['전북', '전라북도', '전북교육청', '전라북도교육청']
-    Jeollanam = ['전남', '전라남도', '전남교육청', '전라남도교육청']
-    Gyeongsangbuk = ['경북', '경상북도', '경북교육청', '경상북도교육청']
-    Gyeongsangnam = ['경남', '경상남도', '경남교육청', '경상남도교육청']
-    Jeju = ['제주', '제주도', '제주특별자치시', '제주교육청', '제주도교육청', '제주특별자치시교육청', '제주특별자치도']
 
     if school_area in Souel:
         return "B10"
@@ -92,10 +72,6 @@ async def get_area_code(school_area:str==None):
 
 #시간표 정보 수집 함수
 async def get_school_timetable(school_code, area_code,day,school_type,school_grade, school_class):
-    school_type_high = ['고등학교', '고','고등']
-    school_type_middle = ['중학교', '중','중등']
-    school_type_elementary = ['초등학교', '초','초등']
-    school_type_special = ['특수학교', '특','특수','특별']
     try:
         if school_type in school_type_high:
             url = f"https://open.neis.go.kr/hub/hisTimetable?KEY={neis_api_key}&Type=json&ATPT_OFCDC_SC_CODE={area_code}&SD_SCHUL_CODE={school_code}&GRADE={school_grade}&CLASS_NM={school_class}&TI_FROM_YMD={day}&TI_TO_YMD={day}"
@@ -112,10 +88,9 @@ async def get_school_timetable(school_code, area_code,day,school_type,school_gra
         print(url)
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
         try:
-            response = requests.get(url)
+            response = requests.get(url,headers=headers)
         except:
-            response = requests.get(url, verify=False)
-        print(response)
+            response = requests.get(url,headers=headers,verify=False)
         timetable_data=json.loads(response.text)
 
         timetable_list = [0 for i in range(len(timetable_data[temp][1]['row']))]
